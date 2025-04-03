21588a1ed32b51b77921a6d5d6ac853c99615245
YamaguchiKoki <shankouhanghui15@gmail.com>
2025-04-03 23:26:53 +0900

    SBIを呼び出しを行う簡易的なカーネルの実装

--- kernel.c
+++ kernel.c
@@ -1,3 +1,5 @@
+#include "kernel.h"
+
 typedef unsigned char uint8_t;
 typedef unsigned int uint32_t;
 typedef uint32_t size_t;
@@ -7,6 +9,42 @@ typedef uint32_t size_t;
 // __stack_top：スタックの末尾アドレス
 extern char __bss[], __bss_end[], __stack_top[];
 
+// S-Mode(カーネル)からM-Mode(OpenSBI)を呼び出す
+struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
+                long arg5, long fid, long eid) {
+    // レジスタに値を設定
+    register long a0 __asm__("a0") = arg0;
+    register long a1 __asm__("a1") = arg1;
+    register long a2 __asm__("a2") = arg2;
+    register long a3 __asm__("a3") = arg3;
+    register long a4 __asm__("a4") = arg4;
+    register long a5 __asm__("a5") = arg5;
+    register long a6 __asm__("a6") = fid;
+    register long a7 __asm__("a7") = eid;
+    // ecall : 上位のモードに制御を移す。例外を発生させて、常駐OpenSBIはそれを捕捉し、OSーハード間の差異を吸収する
+    __asm__ __volatile__("ecall"
+                        : "=r"(a0), "=r"(a1)
+                        : "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a5),
+                          "r"(a6), "r"(a7)
+                        : "memory");
+    return (struct sbiret){.error = a0, .value = a1};
+}
+
+void putchar(char ch) {
+    sbi_call(ch, 0, 0, 0, 0, 0, 0, 1 /* Console Putchar（SBI） */);
+}
+
+void kernel_main(void) {
+    const char *s = "\n\nHello World!\n";
+    for (int i = 0; s[i] != '\0'; i++) {
+        putchar(s[i]);
+    }
+
+    for (;;) {
+        __asm__ __volatile__("wfi");
+    }
+}
+
 // bufの先頭からnバイト分をcで埋める
 void *memset(void *buf, char c, size_t n) {
   // 汎用ポインタを符号なし整数型ポインタにキャスト(1byteずつ処理するため)
@@ -14,11 +52,6 @@ void *memset(void *buf, char c, size_t n) {
   while(n--)
     *p++ = c;
   return buf;
-}
-
-void kernel_main(void) {
-  memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
-  for (;;);
 }
 
 // boot関数をtext.bootセクションに配置する
