d1dd8f2f34d548fae7ab6d8fbcb48cd8f9ee3ff5
YamaguchiKoki <shankouhanghui15@gmail.com>
2025-04-05 13:22:38 +0900

    PANIC実装

--- kernel.h
+++ kernel.h
@@ -4,3 +4,9 @@ struct sbiret {
     long error;
     long value;
 };
+// 複数行マクロを書く時のイディオム（プリプロセッサによるマクロ展開の仕様上、呼び出し方によっては意図しないエラーになるため）
+#define PANIC(fmt, ...) \
+    do { \
+        printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__); \
+        while (1) {} \
+    } while (0)
